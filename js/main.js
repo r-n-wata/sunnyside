7bb88d070bf74076f308f6bc650f678cefb5439e
Ruth Wata <ruthnwata@gmail.com>
2022-08-20 20:31:11 -0500

    changed colors of a

--- js/main.js
+++ js/main.js
@@ -3,9 +3,10 @@
 const openNav = document.querySelector('.open-nav')
 
 const navBar = document.querySelector('.nav-bar')
+navBar.classList.add('hidden')
 
 openNav.addEventListener('click', () =>{
-
+    
     if(!navBar.classList.contains('hidden')){
         navBar.classList.add('hidden')
         
@@ -15,3 +16,5 @@ openNav.addEventListener('click', () =>{
     }
     
 })
+
+
